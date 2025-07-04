185af61449ffe37ac0ce127478364d3340849da5
Lavrud <jr_durval@hotmail.com>
2025-07-03 23:38:37 -0300

    Remove o arquivo `README.md` da API e ajusta o layout da página inicial, alterando o padding e margin do componente principal.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { EmployeeStatusTable } from "@/components/employee-status-table";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-8">
+    <main className="container mx-auto py-4 px-2">
       <h1 className="text-2xl font-bold mb-6">Gerenciamento de Funcionários</h1>
       <EmployeeStatusTable />
     </main>
